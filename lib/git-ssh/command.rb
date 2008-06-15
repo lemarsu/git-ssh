ef96d83c2ece27915b1a23b4ea8b5d35550f5b02
LeMarsu <ch.ruelle@lemarsu.com>
2008-06-15 20:47:08 +0200

    Added command line options

--- lib/git-ssh/command.rb
+++ lib/git-ssh/command.rb
@@ -1,20 +1,19 @@
+require 'optparse'
+
 class GitSsh::Command
 
   COMMANDS_RE = /^(git-(?:receive|upload)-pack) '(.*)'$/o
   ROOT_PATH = "/var/git_repositories"
+  SSH_COMMAND = 'SSH_ORIGINAL_COMMAND'
 
   class UsageError < Exception; end
+  class ShellError < Exception; end
+
+  Options = Struct.new("Options", :verbose, :chroot)
 
   def initialize(*args)
-    raise "What do you think I am? A shell?" unless ENV.keys.include?("SSH_ORIGINAL_COMMAND")
-    if mg = COMMANDS_RE.match(ENV['SSH_ORIGINAL_COMMAND'])
-      @command = mg[1]
-      @path = sanitize_path mg[2]
-    else raise "It seems dangerous"
-    end
-
-    raise UsageError, "#$0 usage: #$0 <user>" unless args.size == 1 && args.first =~ /^\w+$/
-    @user = args.first
+    parse_args(*args)
+    parse_ssh_original_command
   end
 
   def run
@@ -27,6 +26,37 @@ class GitSsh::Command
   end
 
   private
+
+  def parse_args(*args)
+    @args = args
+    @options = Options.new(false, false)
+    parser = get_parser
+    args_left = parser.parse!(@args)
+    raise UsageError, "#$0 usage: #$0 <user>" unless
+      args_left.size == 1 && args_left.first =~ /^\w+$/
+    @user = @args.first
+  end
+
+  def get_parser
+    OptionParser.new do |o|
+      o.banner = "Usage: #$0 [options] <user>"
+
+      o.separator ""
+      o.separator "Options:"
+
+      o.on('-v', '--verbose', 'Be verbose') { @options.verbose = true }
+      o.on '-r DIR', '--chroot=DIR', 'Chroot the user' do |value|
+	@options.chroot = value
+      end
+    end
+  end
+
+  def parse_ssh_original_command
+    raise ShellError, "What do you think I am? A shell?" unless ENV.keys.include?(SSH_COMMAND)
+    raise ShellError, "It seems dangerous" unless mg = COMMANDS_RE.match(ENV[SSH_COMMAND])
+    @command = mg[1]
+    @path = sanitize_path mg[2]
+  end
 
   def sanitize_path(path)
     test_path = path[0] == ?/ ? path : File.join('/', path)
