4ec901fa4cc26d5a4d5d28681b97ba39dd8470fa
LeMarsu <ch.ruelle@lemarsu.com>
2008-06-15 04:56:52 +0200

    removed some debug

--- lib/git-ssh/command.rb
+++ lib/git-ssh/command.rb
@@ -7,12 +7,10 @@ class GitSsh::Command
     raise "What do you think I am? A shell?" unless ENV.keys.include?("SSH_ORIGINAL_COMMAND")
     if mg = COMMANDS_RE.match(ENV['SSH_ORIGINAL_COMMAND'])
       @command = mg[1]
-      puts "before sanitize : #{mg[2]}"
       @path = sanitize_path mg[2]
     else raise "It seems dangerous"
     end
 
-    p [@command, @path]
     unless args.size == 1 && args.first =~ /^\w+$/
       STDERR.puts "#$0 usage: #$0 <user>"
       exit 2
@@ -21,12 +19,8 @@ class GitSsh::Command
   end
 
   def run
-    STDERR.puts "Command line : #{ARGV.inspect}"
-    # STDERR.puts "ENV : #{ENV.inspect}"
-
     path = File.join(ROOT_PATH, @path)
     cmd = [@command, path]
-    STDERR.puts "CMD : #{cmd.join(' ')}"
 
     exec *cmd
     STDERR.puts "Exec sucked!"
@@ -35,7 +29,7 @@ class GitSsh::Command
   private
 
   def sanitize_path(path)
-    # return path unless path[0] == ?/
+    return path unless path[0] == ?/
     File.expand_path(File.join('/', path))
   end
 
